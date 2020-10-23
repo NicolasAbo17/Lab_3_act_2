55ca26fa99daa77a8b96f5fffaa4c7ca5b9b2db7
Andres <af.orozcog@uniandes.edu.co>
2020-10-23 15:56:34 -0500

    lel

--- src/clientePrueba.py
+++ src/clientePrueba.py
@@ -6,21 +6,23 @@ import time
 host_name  = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
 print('HOST IP:',host_ip)
-port = 10002
-BUF_LEN = 4096
+port = 10003
+BUF_LEN = 65540
 #Create socet here
 soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 soc.bind((host_ip, port))
 #Get image size here
 while True:
-    total_pack, addr = soc.recvfrom(4)
+    total_pack, addr = soc.recvfrom(BUF_LEN)
     s=b""
     total_pack = int.from_bytes(total_pack,"little",signed=True)
-    print(total_pack)
+    print("what the fuck?",total_pack)
     for i in range(total_pack):
         data, addr = soc.recvfrom(BUF_LEN)
         s += data
-        print(data)
     frame = numpy.fromstring(s,dtype='uint8')
     frame = cv2.imdecode(frame,0)
-    cv2.imshow('frame',frame)
+    cv2.imshow('recv',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
---
+++ src/lil.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 15:06:02 2020
+
+@author: andre
+"""
+
+
+see = bytearray(10)
